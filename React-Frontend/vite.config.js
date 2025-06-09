3774da794bf5a5e57627caf4292e223ab76f032d
subham007-coder <dsubham109@gmail.com>
2025-06-09 22:32:59 +0530

    react router set up for deploy

--- React-Frontend/vite.config.js
+++ React-Frontend/vite.config.js
@@ -1,18 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: ['.js', '.jsx', '.json']
+    extensions: [".js", ".jsx", ".json"],
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     rollupOptions: {
       output: {
-        manualChunks: undefined
-      }
-    }
-  }
-})
+        manualChunks: undefined,
+      },
+      server: {
+        historyApiFallback: true,
+      },
+    },
+  },
+});
