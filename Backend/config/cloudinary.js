0bead0d13343c776b1d87d74cc6f471177aa11fa
subham007-coder <dsubham109@gmail.com>
2025-06-22 11:27:02 +0530

    prblm i hope fix

--- Backend/config/cloudinary.js
+++ Backend/config/cloudinary.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: 'Nail-Image-DB',
+  cloud_name: 'dplxwyxen',
   api_key: '935135358757851',
   api_secret: 'kO3pvyGQL2Oam9ycXCQb9o4-sG8'
 });
