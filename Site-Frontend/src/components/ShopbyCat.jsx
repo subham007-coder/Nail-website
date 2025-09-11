979b79efdb6a5c5c0c68cbc8c2d4572e88530ad0
subham007-coder <dsubham109@gmail.com>
2025-09-11 22:36:40 +0530

    Shop By Occasion fix the gap againg

--- Site-Frontend/src/components/ShopbyCat.jsx
+++ Site-Frontend/src/components/ShopbyCat.jsx
@@ -43,7 +43,7 @@ function ShopbyCat() {
         Shop By Occasion
       </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         {categories.map((category) => (
           <div
             key={category.id}
